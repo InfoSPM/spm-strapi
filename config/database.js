beb9391244e12ba78324586f4998edf854d192cd
AntixxTechHub <info@antixxtechhub.com>
2022-12-21 13:59:09 +0530

    spm heroku db update 21.12..2

--- config/database.js
+++ config/database.js
@@ -1,15 +1,34 @@
 
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', 'ec2-52-48-159-67.eu-west-1.compute.amazonaws.com'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'dbtn6shd4j5opr'),
+//       user: env('DATABASE_USERNAME', 'uajbchhyhbxuyl'),
+//       password: env('DATABASE_PASSWORD', '35d3c31cd9f00a9639c9e77e6d09971bd02aa242506cb6b2bc6186d778e677f8'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
+
+const parse = require('pg-connection-string').parse;
+const config = parse('postgres://uajbchhyhbxuyl:35d3c31cd9f00a9639c9e77e6d09971bd02aa242506cb6b2bc6186d778e677f8@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dbtn6shd4j5opr');
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'spmglobaltech.postgres.database.azure.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'spmglobaltech'),
-      user: env('DATABASE_USERNAME', 'sqladmin@spmglobaltech'),
-      password: env('DATABASE_PASSWORD', '0!AHW@9Z58u3'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
+    debug: false,
   },
 });
 
